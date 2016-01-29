a38ffa862b3be0d46ecde47c76666eed415ba692
wyy930621 <1146955741@qq.com>
2016-01-30 00:06:31 +0800

    init
    github

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+	// formcontrol.focus = function(){
+	// 	formcontrol.style.borderColor = '#51a7e8';
+	// 	formcontrol.style.boxShadow = 'inset 0 1px 2px rgba(0,0,0,0,075),0 0 5px rgba(81 167 231 0.5)';
+	// }
+};
